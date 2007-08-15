7b6813d3c1d9e3f861d336aa68551fbfc9c6435e
yccheok <devnull@localhost>
2007-08-15 13:01:18 +0000

    gmail setting had been changed.

--- src/org/yccheok/jstock/gui/GoogleMail.java
+++ src/org/yccheok/jstock/gui/GoogleMail.java
@@ -22,6 +22,7 @@
 
 package org.yccheok.jstock.gui;
 
+import com.sun.mail.smtp.SMTPTransport;
 import java.security.Security;
 import java.util.Date;
 import java.util.Properties;
@@ -52,16 +53,24 @@ public class GoogleMail {
         
         // Get a Properties object
         Properties props = System.getProperties();
-        props.setProperty("mail.smtp.host", "smtp.gmail.com");
+        props.setProperty("mail.smtps.host", "smtp.gmail.com");
         props.setProperty("mail.smtp.socketFactory.class", SSL_FACTORY);
         props.setProperty("mail.smtp.socketFactory.fallback", "false");
         props.setProperty("mail.smtp.port", "465");
         props.setProperty("mail.smtp.socketFactory.port", "465");
-        props.put("mail.smtp.auth", "true");
-        Session session = Session.getDefaultInstance(props, new Authenticator(){
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(username, password);
-        }});
+        props.setProperty("mail.smtps.auth", "true");
+        
+        /*
+        If set to false, the QUIT command is sent and the connection is immediately closed. If set 
+        to true (the default), causes the transport to wait for the response to the QUIT command.
+
+        ref :   http://java.sun.com/products/javamail/javadocs/com/sun/mail/smtp/package-summary.html
+                http://forum.java.sun.com/thread.jspa?threadID=5205249
+                smtpsend.java - demo program from javamail
+        */        
+        props.put("mail.smtps.quitwait", "false");
+
+        Session session = Session.getInstance(props, null);
 
         // -- Create a new message --
         Message msg = new MimeMessage(session);
@@ -73,6 +82,14 @@ public class GoogleMail {
         msg.setSubject(title);
         msg.setText(message);
         msg.setSentDate(new Date());
-        Transport.send(msg);        
+        
+        SMTPTransport t =
+            (SMTPTransport)session.getTransport("smtps");
+
+        t.connect("smtp.gmail.com", username, password);
+
+        t.sendMessage(msg, msg.getAllRecipients());
+        
+        t.close();
     }
 }
