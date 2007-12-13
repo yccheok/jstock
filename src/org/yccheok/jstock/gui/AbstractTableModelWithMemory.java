fb26ae9887a7284e53da2bcb449736cf79b2460d
yccheok <devnull@localhost>
2007-12-13 08:09:25 +0000

    support old value memorization.

---
+++ src/org/yccheok/jstock/gui/AbstractTableModelWithMemory.java
@@ -0,0 +1,32 @@
+/*
+ * AbstractTableModelWithMemory.java
+ *
+ * Created on December 4, 2007, 10:19 PM
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Copyright (C) 2007 Cheok YanCheng <yccheok@yahoo.com>
+ */
+
+package org.yccheok.jstock.gui;
+
+/**
+ *
+ * @author yccheok
+ */
+public abstract class AbstractTableModelWithMemory extends javax.swing.table.AbstractTableModel {
+    
+    public abstract Object getOldValueAt(int rowIndex, int columnIndex);
+}
