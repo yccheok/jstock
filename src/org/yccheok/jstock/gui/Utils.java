4b3b0043d266f34c10bc72364a70294794eaba2e
yccheok <devnull@localhost>
2008-04-17 16:25:35 +0000

    deleteDir, toBufferedImage, hasAlpha, getScaledImage

--- src/org/yccheok/jstock/gui/Utils.java
+++ src/org/yccheok/jstock/gui/Utils.java
@@ -22,8 +22,20 @@
 
 package org.yccheok.jstock.gui;
 
+import java.awt.Graphics;
+import java.awt.GraphicsConfiguration;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.HeadlessException;
+import java.awt.Image;
+import java.awt.Transparency;
+import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.PixelGrabber;
+import java.io.File;
 import org.yccheok.jstock.engine.*;
 import java.util.*;
+import javax.swing.ImageIcon;
 
 /**
  *
@@ -35,6 +47,116 @@ public class Utils {
     public Utils() {
     }
     
+    public static java.awt.Image getScaledImage(Image image, int maxWidth, int maxHeight) {        
+        final int imgWidth = image.getWidth(null);
+        final int imgHeight = image.getHeight(null);
+                        
+        final int preferredWidth = Math.min(imgWidth, maxWidth);
+        final int preferredHeight = Math.min(imgHeight, maxHeight);
+        
+        final double scaleX = (double)preferredWidth / (double)imgWidth;
+        final double scaleY = (double)preferredHeight / (double)imgHeight;
+        
+        final double bestScale = Math.min(scaleX, scaleY);
+    
+        return image.getScaledInstance((int)((double)imgWidth * bestScale), (int)((double)imgHeight * bestScale), Image.SCALE_SMOOTH);
+    }
+    
+    // This method returns true if the specified image has transparent pixels
+    private static boolean hasAlpha(Image image) {
+        // If buffered image, the color model is readily available
+        if (image instanceof BufferedImage) {
+            BufferedImage bimage = (BufferedImage)image;
+            return bimage.getColorModel().hasAlpha();
+        }
+    
+        // Use a pixel grabber to retrieve the image's color model;
+        // grabbing a single pixel is usually sufficient
+         PixelGrabber pg = new PixelGrabber(image, 0, 0, 1, 1, false);
+        try {
+            pg.grabPixels();
+        } catch (InterruptedException e) {
+        }
+    
+        // Get the image's color model
+        ColorModel cm = pg.getColorModel();
+        
+        return cm.hasAlpha();
+    }
+    
+    // This method returns a buffered image with the contents of an image
+    public static BufferedImage toBufferedImage(Image image) {
+        if (image instanceof BufferedImage) {
+            return (BufferedImage)image;
+        }
+    
+        // This code ensures that all the pixels in the image are loaded
+        image = new ImageIcon(image).getImage();
+    
+        // Determine if the image has transparent pixels; for this method's
+        // implementation, see e661 Determining If an Image Has Transparent Pixels
+        boolean hasAlpha = hasAlpha(image);
+    
+        // Create a buffered image with a format that's compatible with the screen
+        BufferedImage bimage = null;
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        try {
+            // Determine the type of transparency of the new buffered image
+            int transparency = Transparency.OPAQUE;
+            if (hasAlpha) {
+                transparency = Transparency.BITMASK;
+            }
+    
+            // Create the buffered image
+            GraphicsDevice gs = ge.getDefaultScreenDevice();
+            GraphicsConfiguration gc = gs.getDefaultConfiguration();
+            bimage = gc.createCompatibleImage(
+                image.getWidth(null), image.getHeight(null), transparency);
+        } catch (HeadlessException e) {
+            // The system does not have a screen
+        }
+    
+        if (bimage == null) {
+            // Create a buffered image using the default color model
+            int type = BufferedImage.TYPE_INT_RGB;
+            if (hasAlpha) {
+                type = BufferedImage.TYPE_INT_ARGB;
+            }
+            bimage = new BufferedImage(image.getWidth(null), image.getHeight(null), type);
+        }
+    
+        // Copy image to buffered image
+        Graphics g = bimage.createGraphics();
+    
+        // Paint the image onto the buffered image
+        g.drawImage(image, 0, 0, null);
+        g.dispose();
+    
+        return bimage;
+    }
+    
+    // Deletes all files and subdirectories under dir.
+    // Returns true if all deletions were successful.
+    // If a deletion fails, the method stops attempting to delete and returns false.
+    public static boolean deleteDir(File dir, boolean deleteRoot) {
+        if (dir.isDirectory()) {
+            String[] children = dir.list();
+            for (int i=0; i<children.length; i++) {
+                boolean success = deleteDir(new File(dir, children[i]), true);
+                if (!success) {
+                    return false;
+                }
+            }
+        }
+    
+        // The directory is now empty so delete it
+        if(deleteRoot) {
+            return dir.delete();
+        }
+        
+        return true;
+    }
+
     public static boolean createDirectoryIfDoesNotExist(String directory) {
         java.io.File f = new java.io.File(directory);
         
