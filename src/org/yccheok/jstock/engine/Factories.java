28aa6435e5c76093bfd98941401d0fcb3d0ab6d6
yccheok <devnull@localhost>
2008-11-07 17:37:16 +0000

    Support 10 markets.

--- src/org/yccheok/jstock/engine/Factories.java
+++ src/org/yccheok/jstock/engine/Factories.java
@@ -42,12 +42,37 @@ public enum Factories {
     private static final Map<Country, List<StockServerFactory>> map = new HashMap<Country, List<StockServerFactory>>();
     
     static {
-        List<StockServerFactory> malaysiaList = new ArrayList<StockServerFactory>();
-        List<StockServerFactory> unitedStateList = new ArrayList<StockServerFactory>();
+        final List<StockServerFactory> denmarkList = new ArrayList<StockServerFactory>();
+        final List<StockServerFactory> franceList = new ArrayList<StockServerFactory>();
+        final List<StockServerFactory> germanyList = new ArrayList<StockServerFactory>();
+        final List<StockServerFactory> italyList = new ArrayList<StockServerFactory>();
+        final List<StockServerFactory> malaysiaList = new ArrayList<StockServerFactory>();
+        final List<StockServerFactory> norwayList = new ArrayList<StockServerFactory>();
+        final List<StockServerFactory> spainList = new ArrayList<StockServerFactory>();
+        final List<StockServerFactory> sweedenList = new ArrayList<StockServerFactory>();
+        final List<StockServerFactory> unitedKingdomList = new ArrayList<StockServerFactory>();
+        final List<StockServerFactory> unitedStateList = new ArrayList<StockServerFactory>();
+        
+        denmarkList.add(YahooDenmarkStockServerFactory.newInstance());
+        franceList.add(YahooFranceStockServerFactory.newInstance());
+        germanyList.add(YahooGermanyStockServerFactory.newInstance());
+        italyList.add(YahooItalyStockServerFactory.newInstance());
         malaysiaList.add(CIMBStockServerFactory.newInstance("", ""));
+        norwayList.add(YahooNorwayStockServerFactory.newInstance());
+        spainList.add(YahooSpainStockServerFactory.newInstance());
+        sweedenList.add(YahooSweedenStockServerFactory.newInstance());
+        unitedKingdomList.add(YahooUKStockServerFactory.newInstance());
         unitedStateList.add(YahooUSStockServerFactory.newInstance());
         
+        map.put(Country.Denmark, denmarkList);
+        map.put(Country.France, franceList);
+        map.put(Country.Germany, germanyList);
+        map.put(Country.Italy, italyList);
         map.put(Country.Malaysia, malaysiaList);
+        map.put(Country.Norway, norwayList);
+        map.put(Country.Spain, spainList);
+        map.put(Country.Sweeden, sweedenList);
+        map.put(Country.UnitedKingdom, unitedKingdomList);
         map.put(Country.UnitedState, unitedStateList);
     }
 }
