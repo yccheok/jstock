d75e16aa7114b19a832de381e3c0a19c18df2eec
yccheok <devnull@localhost>
2009-06-02 18:52:59 +0000

    no message

--- src/org/yccheok/jstock/portfolio/Broker.java
+++ src/org/yccheok/jstock/portfolio/Broker.java
@@ -13,7 +13,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- * Copyright (C) 2008 Cheok YanCheng <yccheok@yahoo.com>
+ * Copyright (C) 2009 Yan Cheng Cheok <yccheok@yahoo.com>
  */
 
 package org.yccheok.jstock.portfolio;
