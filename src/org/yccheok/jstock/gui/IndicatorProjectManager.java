182b6d9e46362df464553a820b2aa7a856127acf
yccheok <devnull@localhost>
2009-06-06 17:31:57 +0000

    Remove unused import.

--- src/org/yccheok/jstock/gui/IndicatorProjectManager.java
+++ src/org/yccheok/jstock/gui/IndicatorProjectManager.java
@@ -25,7 +25,6 @@ package org.yccheok.jstock.gui;
 import java.util.*;
 import java.io.*;
 
-import com.thoughtworks.xstream.*;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
