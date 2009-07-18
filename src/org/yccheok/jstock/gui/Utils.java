9dafc6a9b5453882b396b525d168a67724a5d73e
yccheok <devnull@localhost>
2009-07-18 05:31:34 +0000

    1.0.4e

--- src/org/yccheok/jstock/gui/Utils.java
+++ src/org/yccheok/jstock/gui/Utils.java
@@ -714,8 +714,8 @@ public class Utils {
     private static final String APPLICATION_VERSION_STRING = "1.0.4";
 
     // For About box comparision on latest version purpose.
-    // 1.0.4d
-    private static final int APPLICATION_VERSION_ID = 1043;
+    // 1.0.4e
+    private static final int APPLICATION_VERSION_ID = 1044;
 
     private static Executor zombiePool = Executors.newFixedThreadPool(Utils.NUM_OF_THREADS_ZOMBIE_POOL);
 
