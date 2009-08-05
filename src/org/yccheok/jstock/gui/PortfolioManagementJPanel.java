26ac5dc77326fe95d335f74ba5aa1cff2e3f7776
yccheok <devnull@localhost>
2009-08-05 22:30:06 +0000

    updatePrimaryStockServerFactory

--- src/org/yccheok/jstock/gui/PortfolioManagementJPanel.java
+++ src/org/yccheok/jstock/gui/PortfolioManagementJPanel.java
@@ -1,21 +1,20 @@
 /*
- * PortfoliioJPanel.java
+ * JStock - Free Stock Market Software
+ * Copyright (C) 2009 Yan Cheng Cheok <yccheok@yahoo.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or (at
- * your option) any later version.
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- * Copyright (C) 2009 Yan Cheng Cheok <yccheok@yahoo.com>
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 package org.yccheok.jstock.gui;
@@ -1249,9 +1248,15 @@ public class PortfolioManagementJPanel extends javax.swing.JPanel {
 
         return status;
     }
-    
+
+    public void updatePrimaryStockServerFactory(java.util.List<StockServerFactory> stockServerFactories) {
+        if (realTimeStockMonitor != null) {
+            realTimeStockMonitor.setStockServerFactories(stockServerFactories);
+        }
+    }
+
     public void initRealTimeStockMonitor(java.util.List<StockServerFactory> stockServerFactories) {
-        if(realTimeStockMonitor != null) {
+        if (realTimeStockMonitor != null) {
             final RealTimeStockMonitor oldRealTimeStockMonitor = realTimeStockMonitor;
             Utils.getZoombiePool().execute(new Runnable() {
                 @Override
@@ -1266,10 +1271,7 @@ public class PortfolioManagementJPanel extends javax.swing.JPanel {
         }
         
         realTimeStockMonitor = new RealTimeStockMonitor(4, 20, MainFrame.getInstance().getJStockOptions().getScanningSpeed());
-        
-        for(StockServerFactory factory : stockServerFactories) {
-            realTimeStockMonitor.addStockServerFactory(factory);
-        }
+        realTimeStockMonitor.setStockServerFactories(stockServerFactories);
         
         realTimeStockMonitor.attach(this.realTimeStockMonitorObserver);
         
