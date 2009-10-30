c167870996b22eb8063b7cf9e0893475e728904a
yccheok <devnull@localhost>
2009-10-30 08:09:17 +0000

    Added createRSI and createEMA using TA-Lib.

--- src/org/yccheok/jstock/charting/TechnicalAnalysis.java
+++ src/org/yccheok/jstock/charting/TechnicalAnalysis.java
@@ -22,7 +22,7 @@ package org.yccheok.jstock.charting;
 
 import com.tictactec.ta.lib.Core;
 import com.tictactec.ta.lib.MInteger;
-import java.util.Calendar;
+import org.apache.commons.lang.ArrayUtils;
 import org.jfree.data.time.Day;
 import org.jfree.data.time.TimeSeries;
 import org.jfree.data.time.TimeSeriesCollection;
@@ -35,6 +35,46 @@ import org.yccheok.jstock.engine.StockHistoryServer;
  * @author yccheok
  */
 public class TechnicalAnalysis {
+    public static Double createEMA(java.util.List<Double> values, int period) {
+        if (period <= 0) {
+            throw new java.lang.IllegalArgumentException("period must be greater than 0");
+        }
+        final int size = values.size();
+        final Core core = new Core();
+        final int allocationSize = size - core.emaLookback(period);
+        if (allocationSize <= 0) {
+            return null;
+        }
+
+        final double[] output = new double[allocationSize];
+        final MInteger outBegIdx = new MInteger();
+        final MInteger outNbElement = new MInteger();
+        double[] _values = ArrayUtils.toPrimitive(values.toArray(new Double[0]));
+        core.ema(0, values.size() - 1, _values, period, outBegIdx, outNbElement, output);
+
+        return output[outNbElement.value - 1];
+    }
+
+    public static Double createRSI(java.util.List<Double> values, int period) {
+        if (period <= 0) {
+            throw new java.lang.IllegalArgumentException("period must be greater than 0");
+        }
+        final int size = values.size();
+        final Core core = new Core();
+        final int allocationSize = size - core.rsiLookback(period);
+        if (allocationSize <= 0) {
+            return null;
+        }
+
+        final double[] output = new double[allocationSize];
+        final MInteger outBegIdx = new MInteger();
+        final MInteger outNbElement = new MInteger();
+        double[] _values = ArrayUtils.toPrimitive(values.toArray(new Double[0]));
+        core.rsi(0, values.size() - 1, _values, period, outBegIdx, outNbElement, output);
+
+        return output[outNbElement.value - 1];
+    }
+
     public static TimeSeries createEMA(StockHistoryServer stockHistoryServer, String name, int period) {
         if (period <= 0) {
             throw new java.lang.IllegalArgumentException("period must be greater than 0");
