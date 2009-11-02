3c4ed57e502f54edca1ceee0d9768b6b17564aa9
yccheok <devnull@localhost>
2009-11-02 18:03:33 +0000

    launchWebBrowser

--- src/org/yccheok/jstock/gui/Utils.java
+++ src/org/yccheok/jstock/gui/Utils.java
@@ -355,6 +355,30 @@ public class Utils {
         return defaultServer;
     }
 
+    public static void launchWebBrowser(String address) {
+        if (Desktop.isDesktopSupported())
+        {
+            final Desktop desktop = Desktop.getDesktop();
+            if (desktop.isSupported(Desktop.Action.BROWSE))
+            {
+                URL url = null;
+                String string = address;
+                try {
+                    url = new URL(string);
+                } catch (MalformedURLException ex) {
+                    return;
+                }
+                try {
+                    desktop.browse(url.toURI());
+                }
+                catch (URISyntaxException ex) {
+                }
+                catch (IOException ex) {
+                }
+            }
+        }
+    }
+
     public static void launchWebBrowser(javax.swing.event.HyperlinkEvent evt) {
         if (HyperlinkEvent.EventType.ACTIVATED.equals(evt.getEventType())) {
             URL url = evt.getURL();
