fc2a1de242b646e2a3e3d7c5381e6e9e4f91adde
yccheok <devnull@localhost>
2010-02-05 12:46:17 +0000

    Refactor.

--- src/org/yccheok/jstock/portfolio/ActivitySummary.java
+++ src/org/yccheok/jstock/portfolio/ActivitySummary.java
@@ -1,19 +1,20 @@
 /*
+ * JStock - Free Stock Market Software
+ * Copyright (C) 2010 Yan Cheng CHEOK <yccheok@yahoo.com>
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or (at
- * your option) any later version.
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- * Copyright (C) 2009 Yan Cheng Cheok <yccheok@yahoo.com>
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 package org.yccheok.jstock.portfolio;
@@ -32,8 +33,7 @@ import org.yccheok.jstock.engine.SimpleDate;
  */
 public class ActivitySummary {
 
-    public boolean add(SimpleDate date, String who, Activity.Type type, double amount) {
-        Activity activity = new Activity(who, type, amount);
+    public boolean add(SimpleDate date, Activity activity) {
         Activities activities = activitiesMap.get(date);
         if (activities == null) {
             activities = new Activities(date);
@@ -59,7 +59,7 @@ public class ActivitySummary {
         return activitiesMap.values();
     }
 
-   	/* simpleDates shall be removed. It is still here and marked as transient, for xstream backward compatible purpose. */
+    /* simpleDates shall be removed. It is still here and marked as transient, for xstream backward compatible purpose. */
     private transient List<SimpleDate> simpleDates = new ArrayList<SimpleDate>();
     
     private final Map<SimpleDate, Activities> activitiesMap = new HashMap<SimpleDate, Activities>();
