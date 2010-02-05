8b89b2fb25389310ce5e635cf9ee406fe7df1538
yccheok <devnull@localhost>
2010-02-05 21:12:09 +0000

    Remove getNetAmount.

--- src/org/yccheok/jstock/portfolio/Activities.java
+++ src/org/yccheok/jstock/portfolio/Activities.java
@@ -1,26 +1,27 @@
 /*
+ * JStock - Free Stock Market Software
+ * Copyright (C) 2010 Yan Cheng CHEOK <yccheok@yahoo.com>
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or (at
- * your option) any later version.
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- * Copyright (C) 2009 Yan Cheng Cheok <yccheok@yahoo.com>
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 package org.yccheok.jstock.portfolio;
 
-import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -31,7 +32,7 @@ import org.yccheok.jstock.engine.Stock;
  *
  * @author yccheok
  */
-public class Activities implements java.lang.Comparable<Activities> {
+public class Activities {
     public Activities(SimpleDate simpleDate)
     {
         this.simpleDate = simpleDate;
@@ -42,8 +43,14 @@ public class Activities implements java.lang.Comparable<Activities> {
     }
 
     public boolean add(Activity activity) {
-        needEvaluation = true;
-        return this.activities.add(activity);
+        final boolean status = this.activities.add(activity);
+        java.util.Collections.sort(this.activities, new Comparator<Activity>() {
+            @Override
+            public int compare(Activity o1, Activity o2) {
+                return o1.getType().compareTo(o2.getType());
+            }
+        });
+        return status;
     }
 
     public int size() {
@@ -63,35 +70,6 @@ public class Activities implements java.lang.Comparable<Activities> {
         }
         java.util.Collections.sort(types);
         return types;
-    }
-
-    public double getNetAmount() {
-        if (false == needEvaluation) {
-            return netAmount;
-        }
-
-        netAmount = 0.0;
-
-        for (Activity activity : activities) {
-            switch(activity.getType()) {
-                case Buy:
-                    netAmount = netAmount - activity.getAmount();
-                    break;
-                case Sell:
-                    netAmount = netAmount + activity.getAmount();
-                    break;
-                case Deposit:
-                    netAmount = netAmount + activity.getAmount();
-                    break;
-                case Dividend:
-                    netAmount = netAmount + activity.getAmount();
-                    break;
-            }
-        }
-
-        needEvaluation = false;
-
-        return netAmount;
     }
 
     public String toSummary() {
@@ -135,14 +113,6 @@ public class Activities implements java.lang.Comparable<Activities> {
         message = message + "";
         return message;
     }
-
-    @Override
-    public int compareTo(Activities o) {
-        return this.simpleDate.compareTo(o.simpleDate);
-    }
-
-    private double netAmount = 0.0;
-    private volatile boolean needEvaluation = true;
 
     SimpleDateFormat dateFormat = new SimpleDateFormat("d-MMM-yyyy");
     //static {
