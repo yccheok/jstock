f2e1003e4331cfcfa9612e1e52467b3977f65ce0
yccheok <devnull@localhost>
2010-02-05 12:52:26 +0000

    Refactor.

--- src/org/yccheok/jstock/portfolio/Activity.java
+++ src/org/yccheok/jstock/portfolio/Activity.java
@@ -1,31 +1,39 @@
 /*
+ * JStock - Free Stock Market Software
+ * Copyright (C) 2010 Yan Cheng CHEOK <yccheok@yahoo.com>
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or (at
- * your option) any later version.
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- * Copyright (C) 2009 Yan Cheng Cheok <yccheok@yahoo.com>
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 package org.yccheok.jstock.portfolio;
 
-import java.text.NumberFormat;
+import java.util.HashMap;
+import java.util.Map;
 import javax.swing.ImageIcon;
+import org.yccheok.jstock.engine.Stock;
 
 /**
  *
  * @author yccheok
  */
 public class Activity {
+    public enum Param {
+        Stock,
+        Quantity
+    }
+
     public enum Type {
         Deposit("/images/16x16/money.png"),
         Buy("/images/16x16/inbox.png"),
@@ -43,18 +51,39 @@ public class Activity {
         private ImageIcon icon;
     }
 
-    public Activity(String who, Type type, double amount) {
-        this.type = type;
-        this.amount = amount;
-        this.who = who;
+    public static class Builder {
+        // Required parameters
+        private final Type type;
+        private final double amount;
+        private Map<Param, Object> paramToObject = new HashMap<Param, Object>();
+
+        public Builder(Type type, double amount) {
+            this.type = type;
+            this.amount = amount;
+        }
+
+        public Builder put(Param param, Object value) {
+            paramToObject.put(param, value);
+            return this;
+        }
+
+        public Activity build() {
+            return new Activity(this);
+        }
+    }
+
+    private Activity(Builder builder) {
+        this.type = builder.type;
+        this.amount = builder.amount;
+        this.paramToObject = builder.paramToObject;
+    }
+
+    public Object get(Param param) {
+        return paramToObject.get(param);
     }
 
     public Type getType() {
         return this.type;
-    }
-
-    public String getWho() {
-        return who;
     }
 
     public double getAmount() {
@@ -63,19 +92,19 @@ public class Activity {
 
     @Override
     public String toString() {
-        if (who.length() > 1) {
-            return who + " " + type.toString().toLowerCase() + " " + currencyFormat.format(amount);
+        Stock stock = (Stock)this.get(Param.Stock);
+
+        if (stock != null) {
+            return stock.getSymbol().toString() + " " + type.toString().toLowerCase() + " " + Utils.currencyNumberFormat(amount);
         }
 
-        return type.toString().toLowerCase() + " " + currencyFormat.format(amount);
+        return type.toString().toLowerCase() + " " + Utils.currencyNumberFormat(amount);
     }
-
-    private static final NumberFormat currencyFormat = java.text.NumberFormat.getCurrencyInstance();
     //static {
     //    currencyFormat.setMaximumFractionDigits(2);
     //    currencyFormat.setMinimumFractionDigits(2);
     //}
-    private final String who;
     private final Type type;
     private final double amount;
+    private Map<Param, Object> paramToObject = new HashMap<Param, Object>();
 }
