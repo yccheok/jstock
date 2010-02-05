d40672a8b98e6e3b3ab6afc71c47b24868141cef
yccheok <devnull@localhost>
2010-02-05 12:58:56 +0000

    Use Utils.currencyNumberFormat.

--- src/org/yccheok/jstock/gui/table/CurrencyRenderer.java
+++ src/org/yccheok/jstock/gui/table/CurrencyRenderer.java
@@ -1,19 +1,20 @@
 /*
+ * JStock - Free Stock Market Software
+ * Copyright (C) 2010 Yan Cheng CHEOK <yccheok@yahoo.com>
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or (at
- * your option) any later version.
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- * Copyright (C) 2009 Yan Cheng Cheok <yccheok@yahoo.com>
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 package org.yccheok.jstock.gui.table;
@@ -25,17 +26,12 @@ import java.text.NumberFormat;
  * @author yancheng
  */
 public class CurrencyRenderer extends GenericRenderer {
-    private NumberFormat format = null;
-
     public CurrencyRenderer() { 
         super();
     }
 
     @Override
     public void setValue(Object value) {
-        if (format == null) {
-            format = java.text.NumberFormat.getCurrencyInstance();
-        }
-        setText((value == null) ? "" : format.format(value));
+        setText((value == null) ? "" : org.yccheok.jstock.portfolio.Utils.currencyNumberFormat(value));
     }
 }
