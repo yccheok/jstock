279d50e68ec18ade1b8392eac947a8d38666040b
yccheok <devnull@localhost>
2010-03-06 10:51:17 +0000

    Support any type of email alert (Yahoo! Mail, your company mail...), not just GMail email alert. (#2920400)

--- src/org/yccheok/jstock/alert/GoogleMail.java
+++ src/org/yccheok/jstock/alert/GoogleMail.java
@@ -1,187 +1,115 @@
 /*
-
- * GoogleMail.java
-
+ * JStock - Free Stock Market Software
+ * Copyright (C) 2010 Yan Cheng CHEOK <yccheok@yahoo.com>
  *
-
- * Created on April 5, 2007, 11:59 PM
-
- *
-
  * This program is free software; you can redistribute it and/or modify
-
  * it under the terms of the GNU General Public License as published by
-
- * the Free Software Foundation; either version 2 of the License, or (at
-
- * your option) any later version.
-
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
-
- * This program is distributed in the hope that it will be useful, but
-
- * WITHOUT ANY WARRANTY; without even the implied warranty of
-
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-
- * General Public License for more details.
-
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
-
- * You should have received a copy of the GNU General Public License
-
- * along with this program; if not, write to the Free Software
-
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-
- *
-
- * Copyright (C) 2007 Cheok YanCheng <yccheok@yahoo.com>
-
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
-
-
 
 package org.yccheok.jstock.alert;
 
-
-
 import com.sun.mail.smtp.SMTPTransport;
-
 import java.security.Security;
-
 import java.util.Date;
-
 import java.util.Properties;
-
-
-
-
 import javax.mail.Message;
-
 import javax.mail.MessagingException;
-
-
 import javax.mail.Session;
-
-
 import javax.mail.internet.AddressException;
-
 import javax.mail.internet.InternetAddress;
-
 import javax.mail.internet.MimeMessage;
 
-
-
 /**
-
  *
-
  * @author doraemon
-
  */
-
 public class GoogleMail {
-
-    
-
-    /** Creates a new instance of GoogleMail */
-
-    public GoogleMail() {
-
+    private GoogleMail() {
     }
 
-    
-
+    /**
+     * Send email using GMail SMTP server.
+     *
+     * @param username GMail username
+     * @param password GMail password
+     * @param recipientEmail TO recipient
+     * @param title title of the message
+     * @param message message to be sent
+     * @throws AddressException if the email address parse failed
+     * @throws MessagingException if the connection is dead or not in the connected state or if the message is not a MimeMessage
+     */
     public static void Send(final String username, final String password, String recipientEmail, String title, String message) throws AddressException, MessagingException {
+        GoogleMail.Send(username, password, recipientEmail, "", title, message);
+    }
 
+    /**
+     * Send email using GMail SMTP server.
+     *
+     * @param username GMail username
+     * @param password GMail password
+     * @param recipientEmail TO recipient
+     * @param ccEmail CC recipient. Can be empty if there is no CC recipient
+     * @param title title of the message
+     * @param message message to be sent
+     * @throws AddressException if the email address parse failed
+     * @throws MessagingException if the connection is dead or not in the connected state or if the message is not a MimeMessage
+     */
+    public static void Send(final String username, final String password, String recipientEmail, String ccEmail, String title, String message) throws AddressException, MessagingException {
         Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
-
         final String SSL_FACTORY = "javax.net.ssl.SSLSocketFactory";
 
-        
-
         // Get a Properties object
-
         Properties props = System.getProperties();
-
         props.setProperty("mail.smtps.host", "smtp.gmail.com");
-
         props.setProperty("mail.smtp.socketFactory.class", SSL_FACTORY);
-
         props.setProperty("mail.smtp.socketFactory.fallback", "false");
-
         props.setProperty("mail.smtp.port", "465");
-
         props.setProperty("mail.smtp.socketFactory.port", "465");
-
         props.setProperty("mail.smtps.auth", "true");
 
-        
-
         /*
-
         If set to false, the QUIT command is sent and the connection is immediately closed. If set 
-
         to true (the default), causes the transport to wait for the response to the QUIT command.
 
-
-
         ref :   http://java.sun.com/products/javamail/javadocs/com/sun/mail/smtp/package-summary.html
-
                 http://forum.java.sun.com/thread.jspa?threadID=5205249
-
                 smtpsend.java - demo program from javamail
-
-        */        
-
+        */
         props.put("mail.smtps.quitwait", "false");
-
-
 
         Session session = Session.getInstance(props, null);
 
-
-
         // -- Create a new message --
-
         Message msg = new MimeMessage(session);
 
-
-
         // -- Set the FROM and TO fields --
-
         msg.setFrom(new InternetAddress(username + "@gmail.com"));
+        msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipientEmail, false));
 
-        msg.setRecipients(Message.RecipientType.TO,
-
-            InternetAddress.parse(recipientEmail, false));
+        if (ccEmail.length() > 0) {
+            msg.setRecipients(Message.RecipientType.CC, InternetAddress.parse(ccEmail, false));
+        }
 
         msg.setSubject(title);
-
         msg.setText(message);
-
         msg.setSentDate(new Date());
-
         
-
-        SMTPTransport t =
-
-            (SMTPTransport)session.getTransport("smtps");
-
-
+        SMTPTransport t = (SMTPTransport)session.getTransport("smtps");
 
         t.connect("smtp.gmail.com", username, password);
-
-
-
-        t.sendMessage(msg, msg.getAllRecipients());
-
-        
-
+        t.sendMessage(msg, msg.getAllRecipients());      
         t.close();
-
     }
-
 }
 
