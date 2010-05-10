edb970d5b3eba271ced35c645bb3043bb4bbe629
yccheok <devnull@localhost>
2010-05-10 16:56:16 +0000

    *** empty log message ***

--- src/org/yccheok/jstock/gui/AutoCompleteJComboBox.java
+++ src/org/yccheok/jstock/gui/AutoCompleteJComboBox.java
@@ -1,6 +1,6 @@
 /*
  * JStock - Free Stock Market Software
- * Copyright (C) 2009 Yan Cheng Cheok <yccheok@yahoo.com>
+ * Copyright (C) 2010 Yan Cheng CHEOK <yccheok@yahoo.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -96,7 +96,7 @@ public class AutoCompleteJComboBox extends JComboBox {
         
         KeyListener[] listeners = this.getEditor().getEditorComponent().getKeyListeners();
         
-        for(KeyListener listener : listeners) {
+        for (KeyListener listener : listeners) {
             if(listener == keyAdapter) {
                 return;
             }
