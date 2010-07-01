847e34811dc8ed94ed4404086d9e1fe98b66828d
yccheok <devnull@localhost>
2010-07-01 12:28:26 +0000

    *** empty log message ***

--- tools/validate_resource_bundle.pl
+++ tools/validate_resource_bundle.pl
@@ -10,7 +10,7 @@ my $dir = "c:/Projects/jstock/src";
 
 if ($ARGV[0])
 {
-	$dir = $ARGV[0];
+    $dir = $ARGV[0];
 }
 
 find(\&init, $dir);
@@ -21,85 +21,80 @@ print "$process_counter files being processed.\n";
 
 sub init
 {
-	my $name = $_;
-	my $dir = $File::Find::dir;
-	my $file = $File::Find::name;	
-	if ($file !~ /(gui\.properties$)|(messages\.properties$)/) {
-		return;
-	}
-	
-	my $hash = \%gui_bundle;
-	if ($file =~ /(messages\.properties$)/) {
-		$hash = \%messages_bundle;
-	}
-	open FILE, $file or die $!;
-	while (my $line = <FILE>) {
-		if ($line =~ /^#/) {
-			next;
-		}
-		chomp $line;
+    my $name = $_;
+    my $dir = $File::Find::dir;
+    my $file = $File::Find::name;	
+    if ($file !~ /(gui\.properties$)|(messages\.properties$)/) {
+        return;
+    }
 
-		if ($line =~ /([^=]+)=(.+)/) {
-			my $key = $1;
-			my $value = $2;
-			if ($hash->{$key}) {
-				print "WARNING : conflict occurs for key '$key'\n";
-			}
-			$hash->{$key} = $value;
-			$init_counter++;
-		}
-	}
-	close(FILE);	
+    my $hash = \%gui_bundle;
+    if ($file =~ /(messages\.properties$)/) {
+        $hash = \%messages_bundle;
+    }
+    open FILE, $file or die $!;
+    while (my $line = <FILE>) {
+        if ($line =~ /^#/) {
+            next;
+        }
+        chomp $line;
+
+        if ($line =~ /([^=]+)=(.+)/) {
+            my $key = $1;
+            my $value = $2;
+            if ($hash->{$key}) {
+                print "WARNING : conflict occurs for key '$key'\n";
+            }
+            $hash->{$key} = $value;
+            $init_counter++;
+        }
+    }
+    close(FILE);
 }
 
 sub process
 {
-	my $name = $_;
-	my $dir = $File::Find::dir;
-	my $file = $File::Find::name;	
-	if ($file !~ /\.java$/) {
-		return;
-	}
-	open FILE, $file or die $!;
-	my $line_number = 0;
-	while (my $line = <FILE>) {
-		$line_number++;
-		if ($line =~ /^#/) {
-			next;
-		}
-		chomp $line;
+    my $name = $_;
+    my $dir = $File::Find::dir;
+    my $file = $File::Find::name;	
+    if ($file !~ /\.java$/) {
+        return;
+    }
+    open FILE, $file or die $!;
+    while (my $line = <FILE>) {
+        chomp $line;
 
-		while ($line =~ /GUIBundle\.getString\("([^"]+)"\)/g) {
-			my $key = $1;
-			if (not $gui_bundle{$key}) {
-				print "WARNING : key '$key' not found ($name $line_number)\n";
-			}
-		}
-		while ($line =~ /MessagesBundle\.getString\("([^"]+)"\)/g) {
-			my $key = $1;
-			if (not $messages_bundle{$key}) {
-				print "WARNING : key '$key' not found ($name $line_number)\n";
-			}		
-		}
+        while ($line =~ /GUIBundle\.getString\("([^"]+)"\)/g) {
+            my $key = $1;
+            if (not $gui_bundle{$key}) {
+                print "WARNING : key '$key' not found ($name $.)\n";
+            }
+        }
+        while ($line =~ /MessagesBundle\.getString\("([^"]+)"\)/g) {
+            my $key = $1;
+            if (not $messages_bundle{$key}) {
+                print "WARNING : key '$key' not found ($name $.)\n";
+            }
+        }
         while ($line =~ /bundle\.getString\("([^"]+)"\)/g) {
             my $key = $1;
-			if (not $messages_bundle{$key} and not $gui_bundle{$key}) {
-				print "WARNING : key '$key' not found ($name $line_number)\n";
-			}            
+            if (not $messages_bundle{$key} and not $gui_bundle{$key}) {
+                print "WARNING : key '$key' not found ($name $.)\n";
+            }            
         }
         while ($line =~ /getBundle\("org\/yccheok\/jstock\/data\/gui"\)\.getString\("([^"]+)"\)/g) {
-			my $key = $1;
-			if (not $gui_bundle{$key}) {
-				print "WARNING : key '$key' not found ($name $line_number)\n";
-			}        
+            my $key = $1;
+            if (not $gui_bundle{$key}) {
+                print "WARNING : key '$key' not found ($name $.)\n";
+            }        
         }
         while ($line =~ /getBundle\("org\/yccheok\/jstock\/data\/messages"\)\.getString\("([^"]+)"\)/g) {
-			my $key = $1;
-			if (not $messages_bundle{$key}) {
-				print "WARNING : key '$key' not found ($name $line_number)\n";
-			}        
+            my $key = $1;
+            if (not $messages_bundle{$key}) {
+                print "WARNING : key '$key' not found ($name $.)\n";
+            }        
         }        
-	}
-	close(FILE);
-	$process_counter++;
+    }
+    close(FILE);
+    $process_counter++;
 }
