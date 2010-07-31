ac83586f0e3d08f204918c296ed334699bcaf220
yccheok <devnull@localhost>
2010-07-31 08:59:54 +0000

    SSEC

--- src/org/yccheok/jstock/engine/Index.java
+++ src/org/yccheok/jstock/engine/Index.java
@@ -1,19 +1,20 @@
 /*
+ * JStock - Free Stock Market Software
+ * Copyright (C) 2010 Yan Cheng CHEOK <yccheok@yahoo.com>
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or (at
- * your option) any later version.
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- * Copyright (C) 2008 Cheok YanCheng <yccheok@yahoo.com>
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 package org.yccheok.jstock.engine;
@@ -50,8 +51,9 @@ public enum Index {
     PSI20("PSI 20", Code.newInstance("^PSI20")),
     TWII("TSEC weighted index", Code.newInstance("^TWII")),
     SSMI("Swiss Market", Code.newInstance("^SSMI")),
-    BVSP("Bovespa", Code.newInstance("^BVSP"));
-
+    BVSP("Bovespa", Code.newInstance("^BVSP")),
+    SSEC("China Shanghai Composite", Code.newInstance("^SSEC"));
+    
     Index(String name, Code code) {
         this.name = name;
         this.code = code;
