f838f47866e454a1a29c88871582b1621ad3d825
yccheok <devnull@localhost>
2010-08-15 16:37:24 +0000

    isNameImmutable

---
+++ src/org/yccheok/jstock/engine/StockNameDatabase.java
@@ -0,0 +1,31 @@
+/*
+ * JStock - Free Stock Market Software
+ * Copyright (C) 2010 Yan Cheng CHEOK <yccheok@yahoo.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package org.yccheok.jstock.engine;
+
+/**
+ * This class is used to provide stock code to stock name mapping, as some
+ * stock server doesn't return good stock name. For example, China users wish
+ * to view stock name in Chinese. However, Yahoo! Stock Server returns stock
+ * name in English. In this case, we need to retrieve stock name from
+ * <code>StockNameDatabase</code>.
+ */
+public class StockNameDatabase {
+
+}
