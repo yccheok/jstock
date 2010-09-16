79b53b7ed7bbc4083471fda607a4742c91dd5e47
yccheok <devnull@localhost>
2010-09-16 06:47:01 +0000

    Initial creation to overcome the following problem :
    Yahoo! Finance no longer provide current information for Dow Jones Index. We will be getting the information from Google Finance.

---
+++ src/org/yccheok/jstock/engine/GoogleStockServerFactory.java
@@ -0,0 +1,93 @@
+/*
+ * JStock - Free Stock Market Software
+ * Copyright (C) 2010 Yan Cheng CHEOK <yccheok@yahoo.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package org.yccheok.jstock.engine;
+
+/**
+ * Provides all Google servers, by using abstract factory pattern. Currently,
+ * we only support MarketServer. For rest of the servers, we will either return
+ * null or empty server.
+ *
+ * @author yccheok
+ */
+public class GoogleStockServerFactory implements StockServerFactory {
+
+    private GoogleStockServerFactory(Country country) {
+        marketServer = new GoogleMarketServer(country);
+    }
+
+    /**
+     * Returns GoogleStockServerFactory based on given country.
+     *
+     * @param country the country
+     * @return GoogleStockServerFactory based on given country
+     */
+    public static StockServerFactory newInstance(Country country) {
+        return new GoogleStockServerFactory(country);
+    }
+
+    /**
+     * Returns stock server for this factory.
+     *
+     * @return stock server for this factory
+     */
+    @Override
+    public StockServer getStockServer() {
+        return Utils.emptyStockServer();
+    }
+
+    /**
+     * Returns stock history server for this factory based on given code. <code>
+     * null</code> will be returned if fail.
+     *
+     * @param code the code
+     * @return stock history server for this factory based on given code. <code>
+     * null</code> will be returned if fail
+     */
+    @Override
+    public StockHistoryServer getStockHistoryServer(Code code) {
+        return null;
+    }
+
+    /**
+     * Returns stock history server for this factory based on given code and 
+     * duration. <code>null</code> will be returned if fail.
+     * 
+     * @param code the code
+     * @param duration the duration
+     * @return stock history server for this factory based on given code and 
+     * duration. <code>null</code> will be returned if fail
+     */
+    @Override
+    public StockHistoryServer getStockHistoryServer(Code code, Duration duration) {
+        return null;
+    }
+
+    /**
+     * Returns market server for this factory.
+     *
+     * @return market server for this factory
+     */
+    @Override
+    public MarketServer getMarketServer() {
+        return marketServer;
+    }
+
+    private final MarketServer marketServer;
+}
