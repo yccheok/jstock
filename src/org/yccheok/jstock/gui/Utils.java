d3d10fe8c5acf7d57454a386b7a9ca449d8d8111
yccheok <devnull@localhost>
2010-09-18 20:28:23 +0000

    *** empty log message ***

--- src/org/yccheok/jstock/gui/Utils.java
+++ src/org/yccheok/jstock/gui/Utils.java
@@ -1155,7 +1155,7 @@ public class Utils {
         if (applicationVersionID == APPLICATION_VERSION_ID) {
             return true;
         }
-        else if (applicationVersionID >= 1051 && applicationVersionID <= 1065) {
+        else if (applicationVersionID >= 1051 && applicationVersionID <= 1066) {
             return true;
         }
       
@@ -2172,9 +2172,9 @@ public class Utils {
     private static final String APPLICATION_VERSION_STRING = "1.0.5";
 
     // For About box comparision on latest version purpose.
-    // 1.0.5q
+    // 1.0.5r
     // Remember to update isCompatible method.
-    private static final int APPLICATION_VERSION_ID = 1066;
+    private static final int APPLICATION_VERSION_ID = 1067;
 
     private static Executor zombiePool = Executors.newFixedThreadPool(Utils.NUM_OF_THREADS_ZOMBIE_POOL);
 
