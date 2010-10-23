c1f48cdf76659a62689ccbadc4077693aec71cae
yccheok <devnull@localhost>
2010-10-23 17:37:57 +0000

    deriveWithSymbol and deriveWithName.

--- src/org/yccheok/jstock/engine/AjaxYahooSearchEngine.java
+++ src/org/yccheok/jstock/engine/AjaxYahooSearchEngine.java
@@ -142,19 +142,19 @@ public class AjaxYahooSearchEngine implements SearchEngine<AjaxYahooSearchEngine
         /**
          * The stock exchange.
          */
-        public final String exch = null;
+        public final String exch;
         /**
          * The stock type.
          */
-        public final String type = null;
+        public final String type;
         /**
          * The stock exchange displayed name.
          */
-        public final String exchDisp = null;
+        public final String exchDisp;
         /**
          * The stock type displayed name.
          */
-        public final String typeDisp = null;
+        public final String typeDisp;
 
         /**
          * Creates a new instance of ResultType, with the specified symbol and
@@ -164,8 +164,7 @@ public class AjaxYahooSearchEngine implements SearchEngine<AjaxYahooSearchEngine
          * @param name The name
          */
         public ResultType(String symbol, String name) {
-            this.symbol = symbol;
-            this.name = name;
+            this(symbol, name, null, null, null, null);
         }
 
         /**
@@ -173,8 +172,24 @@ public class AjaxYahooSearchEngine implements SearchEngine<AjaxYahooSearchEngine
          * properly.
          */
         public ResultType() {
-            this.symbol = null;
-            this.name = null;
+            this(null, null, null, null, null, null);
+        }
+
+        private ResultType(String symbol, String name, String exch, String type, String exchDisp, String typeDisp) {
+            this.symbol = symbol;
+            this.name = name;
+            this.exch = exch;
+            this.type = type;
+            this.exchDisp = exchDisp;
+            this.typeDisp = typeDisp;
+        }
+
+        public ResultType deriveWithSymbol(String symbol) {
+            return new ResultType(symbol, this.name, this.exch, this.type, this.exchDisp, this.typeDisp);
+        }
+
+        public ResultType deriveWithName(String name) {
+            return new ResultType(this.symbol, name, this.exch, this.type, this.exchDisp, this.typeDisp);
         }
 
         @Override
