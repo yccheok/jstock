5c766e4a539dc3dcc73cc50fe619c6777cfcccda
Yan Cheng CHEOK <yccheok@yahoo.com>
2014-06-14 09:00:04 +0800

    Initial creation.

---
+++ src/org/yccheok/jstock/engine/PriceSource.java
@@ -0,0 +1,8 @@
+package org.yccheok.jstock.engine;
+
+public enum PriceSource {
+    // It is important that the used name shall be tally with StockServerFactory
+    // concrete class name.
+    Yahoo,
+    Google;
+}
