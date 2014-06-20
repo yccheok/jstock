1fd42312905cc1fc4f6ba3f2a81c9b63e25e3a30
Yan Cheng CHEOK <yccheok@yahoo.com>
2014-06-20 18:02:39 +0800

    Remove unused import.

--- src/org/yccheok/jstock/engine/BucketList.java
+++ src/org/yccheok/jstock/engine/BucketList.java
@@ -24,8 +24,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
