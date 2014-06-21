7bc85de8f857d0364668609e38f054c1206bf781
Yan Cheng CHEOK <yccheok@yahoo.com>
2014-06-21 12:43:21 +0800

    Added unit test for remove.

--- test/org/yccheok/jstock/engine/CodeBucketListsTest.java
+++ test/org/yccheok/jstock/engine/CodeBucketListsTest.java
@@ -50,29 +50,29 @@ public class CodeBucketListsTest extends TestCase {
         instance.add(Code.newInstance("4.to"));
         
         List<Code> codes = instance.get(0);
-        assertEquals(codes.size(), 3);
+        assertEquals(3, codes.size());
         assertEquals(Code.newInstance("0.kl"), codes.get(0));
         assertEquals(Code.newInstance("1.kl"), codes.get(1));
         assertEquals(Code.newInstance("2.kl"), codes.get(2));
         
         codes = instance.get(1);
-        assertEquals(codes.size(), 1);
+        assertEquals(1, codes.size());
         assertEquals(Code.newInstance("3.kl"), codes.get(0));
         
         codes = instance.get(2);
-        assertEquals(codes.size(), 3);
+        assertEquals(3, codes.size());
         assertEquals(Code.newInstance("0"), codes.get(0));
         assertEquals(Code.newInstance("1.l"), codes.get(1));
         assertEquals(Code.newInstance("2"), codes.get(2));
         
         codes = instance.get(3);
-        assertEquals(codes.size(), 3);
+        assertEquals(3, codes.size());
         assertEquals(Code.newInstance("0.nz"), codes.get(0));
         assertEquals(Code.newInstance("1.to"), codes.get(1));
         assertEquals(Code.newInstance("2.nz"), codes.get(2));
         
         codes = instance.get(4);
-        assertEquals(codes.size(), 2);
+        assertEquals(2, codes.size());
         assertEquals(Code.newInstance("3.nz"), codes.get(0));
         assertEquals(Code.newInstance("4.to"), codes.get(1));         
         
@@ -113,6 +113,114 @@ public class CodeBucketListsTest extends TestCase {
         status = instance.add(Code.newInstance("0.kl"));
         
         assertEquals(false, status);
+    }
+
+    /**
+     * Test of remove method, of class CodeBucketLists.
+     */
+    public void testRemove() {
+        System.out.println("remove");
+        
+        CodeBucketLists instance = new CodeBucketLists(3);
+        instance.add(Code.newInstance("0.kl"));
+        instance.add(Code.newInstance("0"));
+        instance.add(Code.newInstance("1.kl"));
+        instance.add(Code.newInstance("1.l"));
+        instance.add(Code.newInstance("2.kl"));
+        instance.add(Code.newInstance("3.kl"));
+        instance.add(Code.newInstance("0.nz"));
+        instance.add(Code.newInstance("1.to"));
+        instance.add(Code.newInstance("2"));
+        instance.add(Code.newInstance("2.nz"));
+        instance.add(Code.newInstance("3.nz"));
+        instance.add(Code.newInstance("4.to"));
+        
+        boolean status = instance.remove(Code.newInstance("no_such_code"));
+        assertEquals(false, status);
+        
+        assertEquals(5, instance.size());
+        
+        status = instance.remove(Code.newInstance("1.kl"));
+        assertEquals(true, status);
+
+        assertEquals(4, instance.size());
+        
+        status = instance.remove(Code.newInstance("1.kl"));
+        assertEquals(false, status);
+        
+        List<Code> codes = instance.get(0);
+        assertEquals(3, codes.size());
+        assertEquals(Code.newInstance("0.kl"), codes.get(0));
+        assertEquals(Code.newInstance("2.kl"), codes.get(1));
+        assertEquals(Code.newInstance("3.kl"), codes.get(2));
+                
+        codes = instance.get(1);
+        assertEquals(3, codes.size());
+        assertEquals(Code.newInstance("0"), codes.get(0));
+        assertEquals(Code.newInstance("1.l"), codes.get(1));
+        assertEquals(Code.newInstance("2"), codes.get(2));
+        
+        codes = instance.get(2);
+        assertEquals(3, codes.size());
+        assertEquals(Code.newInstance("0.nz"), codes.get(0));
+        assertEquals(Code.newInstance("1.to"), codes.get(1));
+        assertEquals(Code.newInstance("2.nz"), codes.get(2));
+        
+        codes = instance.get(3);
+        assertEquals(2, codes.size());
+        assertEquals(Code.newInstance("3.nz"), codes.get(0));
+        assertEquals(Code.newInstance("4.to"), codes.get(1)); 
+        
+        status = instance.remove(Code.newInstance("0"));
+        assertEquals(true, status);
+        assertEquals(4, instance.size());
+        
+        codes = instance.get(0);
+        assertEquals(3, codes.size());
+        assertEquals(Code.newInstance("0.kl"), codes.get(0));
+        assertEquals(Code.newInstance("2.kl"), codes.get(1));
+        assertEquals(Code.newInstance("3.kl"), codes.get(2));
+                
+        codes = instance.get(1);
+        assertEquals(2, codes.size());
+        assertEquals(Code.newInstance("1.l"), codes.get(0));
+        assertEquals(Code.newInstance("2"), codes.get(1));
+        
+        codes = instance.get(2);
+        assertEquals(3, codes.size());
+        assertEquals(Code.newInstance("0.nz"), codes.get(0));
+        assertEquals(Code.newInstance("1.to"), codes.get(1));
+        assertEquals(Code.newInstance("2.nz"), codes.get(2));
+        
+        codes = instance.get(3);
+        assertEquals(2, codes.size());
+        assertEquals(Code.newInstance("3.nz"), codes.get(0));
+        assertEquals(Code.newInstance("4.to"), codes.get(1));   
+        
+        status = instance.remove(Code.newInstance("2"));
+        assertEquals(true, status);
+        assertEquals(4, instance.size());
+        
+        codes = instance.get(0);
+        assertEquals(3, codes.size());
+        assertEquals(Code.newInstance("0.kl"), codes.get(0));
+        assertEquals(Code.newInstance("2.kl"), codes.get(1));
+        assertEquals(Code.newInstance("3.kl"), codes.get(2));
+                
+        codes = instance.get(1);
+        assertEquals(1, codes.size());
+        assertEquals(Code.newInstance("1.l"), codes.get(0));
+        
+        codes = instance.get(2);
+        assertEquals(3, codes.size());
+        assertEquals(Code.newInstance("0.nz"), codes.get(0));
+        assertEquals(Code.newInstance("1.to"), codes.get(1));
+        assertEquals(Code.newInstance("2.nz"), codes.get(2));
+        
+        codes = instance.get(3);
+        assertEquals(2, codes.size());
+        assertEquals(Code.newInstance("3.nz"), codes.get(0));
+        assertEquals(Code.newInstance("4.to"), codes.get(1));          
     }
     
 }
