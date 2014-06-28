3eb0ac5ebec0f522ff8eb7aba5e4c5820ff393b0
Yan Cheng CHEOK <yccheok@yahoo.com>
2014-06-28 23:40:53 +0800

    1.0.7o

--- src/org/yccheok/jstock/gui/Utils.java
+++ src/org/yccheok/jstock/gui/Utils.java
@@ -1,6 +1,6 @@
 /*
  * JStock - Free Stock Market Software
- * Copyright (C) 2012 Yan Cheng CHEOK <yccheok@yahoo.com>
+ * Copyright (C) 2014 Yan Cheng Cheok <yccheok@yahoo.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -2742,15 +2742,15 @@ public class Utils {
     // We will use this as directory name. Do not have space or special characters.
     private static final String APPLICATION_VERSION_STRING = "1.0.7";
 
-    private static final String ABOUT_BOX_VERSION_STRING = "1.0.7n";
+    private static final String ABOUT_BOX_VERSION_STRING = "1.0.7o";
     
     // 1.0.7e
     // Remember to update isCloudFileCompatible method.
     private static final int CLOUD_FILE_VERSION_ID = 1107;
 
-    // 1.0.7n
+    // 1.0.7o
     // For About box comparision on latest version purpose.
-    private static final int APPLICATION_VERSION_ID = 1117;
+    private static final int APPLICATION_VERSION_ID = 1118;
         
     private static final Executor zombiePool = Executors.newFixedThreadPool(Utils.NUM_OF_THREADS_ZOMBIE_POOL);
 
