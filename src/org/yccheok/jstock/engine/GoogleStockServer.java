1ff25fd65664450ca3d662e8939fa5d0096ad4de
Yan Cheng CHEOK <yccheok@yahoo.com>
2014-07-02 01:48:33 +0800

    Let's say London stock exchange & OTCMKTS stock exchange both contains LON:ENVS and OTCMKTS:ENVS respectively. Making query using "ENVS" will return LON:ENVS. In such case, we need to perform some special treaty.

--- src/org/yccheok/jstock/engine/GoogleStockServer.java
+++ src/org/yccheok/jstock/engine/GoogleStockServer.java
@@ -29,6 +29,7 @@ import java.util.Map;
 import java.util.Set;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.yccheok.jstock.gui.Pair;
 
 /**
  *
@@ -80,16 +81,13 @@ public class GoogleStockServer implements StockServer {
         try {
             // Exception will be thrown from apache httpclient, if we do not
             // perform URL encoding.
-            Code _code = codes.get(0);
-            String googleFormat = Utils.toGoogleFormat(_code).toString();
-            builder.append(java.net.URLEncoder.encode(Utils.toGoogleFormat(_code).toString(), "UTF-8"));
-            originalCodes.put(googleFormat, _code);
-
-            for (int i = 1, size = codes.size(); i < size; i++) {
-                builder.append(",");
-                _code = codes.get(i);
-                googleFormat = Utils.toGoogleFormat(_code).toString();
-                builder.append(java.net.URLEncoder.encode(Utils.toGoogleFormat(_code).toString(), "UTF-8"));
+            for (int i = 0, size = codes.size(); i < size; i++) {
+                if (i > 0) {
+                    builder.append(",");
+                }
+                Code _code = codes.get(i);
+                String googleFormat = Utils.toGoogleFormat(_code).toString();
+                builder.append(java.net.URLEncoder.encode(googleFormat, "UTF-8"));
                 originalCodes.put(googleFormat, _code);
             }
             
@@ -108,95 +106,49 @@ public class GoogleStockServer implements StockServer {
             }
             
             final List<Stock> stocks = new ArrayList<Stock>();
+            Set<Code> currCodes = new HashSet<Code>();
+            
+            // Let's say London stock exchange & OTCMKTS stock exchange both
+            // contains LON:ENVS and OTCMKTS:ENVS respectively. Making query 
+            // using "ENVS" will return LON:ENVS. In such case, we need to
+            // perform some special treaty.
+            List<Stock> specialUSStocks = new ArrayList<Stock>();
+            
             for (int i = 0, size = jsonArray.size(); i < size; i++) {
                 final Map<String, String> jsonObject = jsonArray.get(i);
-                String name;
-                final String ticker;
-                final String exchange;
-                
-                try {
-                    name = jsonObject.get("name");
-                    name = name.substring(0, Math.min(SYMBOL_MAX_LENGTH, name.length())).trim();
-                    ticker = jsonObject.get("t").toUpperCase();
-                    exchange = jsonObject.get("e").toUpperCase();
-                } catch (Exception ex) {
-                    log.error(null, ex);
-                    continue;
-                }
-                
-                Code code = getOriginalCode(originalCodes, ticker, exchange);
-                if (code == null) {
-                    continue;
-                }
-                
-                double c = 0;
-                double l = 0;
-                double p = 0;
-                double op = 0;
-                double hi = 0;
-                double lo = 0;
-                long vo = 0;
-                double cp = 0;
-                
-                // Change
-                try { c = Double.parseDouble(jsonObject.get("c").replaceAll("[^0-9\\.\\-]", "")); } catch (NumberFormatException ex) { log.error(null, ex); }
-                // Last
-                try {l = Double.parseDouble(jsonObject.get("l").replaceAll("[^0-9\\.]", "")); } catch (NumberFormatException ex) { log.error(null, ex); }
-                // Prev
-                try { p = l - c; } catch (NumberFormatException ex) { log.error(null, ex); }
-                // Open
-                try { op = Double.parseDouble(jsonObject.get("op").replaceAll("[^0-9\\.\\-]", "")); } catch (NumberFormatException ex) { log.error(null, ex); }
-                // High
-                try { hi = Double.parseDouble(jsonObject.get("hi").replaceAll("[^0-9\\.]", "")); } catch (NumberFormatException ex) { log.error(null, ex); }
-                // Low
-                try { lo = Double.parseDouble(jsonObject.get("lo").replaceAll("[^0-9\\.]", "")); } catch (NumberFormatException ex) { log.error(null, ex); }
-                // Vol
-                try { 
-                    String vo_string = jsonObject.get("vo");
-                    vo = (long)Double.parseDouble(vo_string.replaceAll("[^0-9\\.]", "")); 
-                    if (vo_string.endsWith("K")) {
-                        vo = vo * 1000;
-                    } else if (vo_string.endsWith("M")) {
-                        vo = vo * 1000000;
-                    } else if (vo_string.endsWith("B")) {
-                        vo = vo * 1000000000;
+                Pair<Stock, Boolean> stockEx = toStockEx(jsonObject, originalCodes);
+                if (stockEx != null) {
+                    final Stock stock = stockEx.first;
+                    if (stockEx.second) {
+                        if (false == currCodes.contains(stock.code)) {
+                            stocks.add(stock);
+                            currCodes.add(stock.code);
+                        }
+                    } else {
+                        specialUSStocks.add(stockEx.first);
                     }
-                } catch (NumberFormatException ex) { log.error(null, ex); }
-                // Change Percentage
-                try { cp = Double.parseDouble(jsonObject.get("cp").replaceAll("[^0-9\\.\\-]", "")); } catch (NumberFormatException ex) { log.error(null, ex); }
-                // No last volumne information for Google Finance.
-                // No buy price information for Google Finance.
-                // No buy volume information for Google Finance.
-                // No sell price information for Google Finance.
-                // No sell volume information for Google Finance.                    
-                final Stock stock = new Stock.Builder(code, Symbol.newInstance(name))
-                        .name(name)
-                        .changePrice(c)
-                        .lastPrice(l)
-                        .prevPrice(p)
-                        .openPrice(op)
-                        .highPrice(hi)
-                        .lowPrice(lo)
-                        .volume(vo)
-                        .changePricePercentage(cp)
-                        .build();
-                stocks.add(stock);
+                }
             } 
 
-            Set<Code> currCodes = new HashSet<Code>();
-            List<Stock> emptyStocks = new ArrayList<Stock>();
-
-            for (Stock stock : stocks) {
-                currCodes.add(stock.code);
+            // Special US stock handling.
+            List<Code> specialUSCodes = new ArrayList<Code>();
+            for (Stock stock : specialUSStocks) {
+                specialUSCodes.add(stock.code);
+            }
+            specialUSStocks = getSpecialUSStocks(specialUSCodes);
+            
+            for (Stock stock : specialUSStocks) {
+                if (false == currCodes.contains(stock.code)) {
+                    stocks.add(stock);
+                    currCodes.add(stock.code);
+                }
             }
 
             for (Code code : codes) {
-                if (currCodes.contains(code) == false) {
-                    emptyStocks.add(org.yccheok.jstock.engine.Utils.getEmptyStock(code, Symbol.newInstance(code.toString())));
+                if (false == currCodes.contains(code)) {
+                    stocks.add(org.yccheok.jstock.engine.Utils.getEmptyStock(code, Symbol.newInstance(code.toString())));
                 }
             }
-
-            stocks.addAll(emptyStocks);
 
             if (stocks.size() != codes.size()) {
                 throw new StockNotFoundException("Stock size (" + stocks.size() + ") inconsistent with code size (" + codes.size() + ")");
@@ -210,6 +162,178 @@ public class GoogleStockServer implements StockServer {
             // in the JSON string.
             throw new StockNotFoundException(null, ex);
         }
+    }
+    
+    private List<Stock> getSpecialUSStocks(List<Code> codes) {
+        if (codes.isEmpty()) {
+            return java.util.Collections.emptyList();
+        }
+        
+        Map<String, Code> originalCodes = new HashMap<String, Code>();
+                
+        // Use StringBuilder instead of StringBuffer. We do not concern on 
+        // thread safety.
+        final StringBuilder builder = new StringBuilder("https://www.google.com/finance/info?infotype=infoquoteall&q=");
+        try {
+            // Exception will be thrown from apache httpclient, if we do not
+            // perform URL encoding.
+            for (int i = 0, size = codes.size(); i < size; i++) {
+                if (i > 0) {
+                    builder.append(",");
+                }
+                Code _code = codes.get(i);
+                
+                String googleFormat0 = "NASDAQ:" + _code;
+                String googleFormat1 = "NYSE:" + _code;
+                String googleFormat2 = "NYSEARCA:" + _code;
+                String googleFormat3 = "NYSEMKT:" + _code;
+                String googleFormat4 = "OPRA:" + _code;
+                String googleFormat5 = "OTCBB:" + _code;
+                String googleFormat6 = "OTCMKTS:" + _code;
+                originalCodes.put(googleFormat0, _code);
+                originalCodes.put(googleFormat1, _code);
+                originalCodes.put(googleFormat2, _code);
+                originalCodes.put(googleFormat3, _code);
+                originalCodes.put(googleFormat4, _code);
+                originalCodes.put(googleFormat5, _code);
+                originalCodes.put(googleFormat6, _code);
+                
+                builder.append(java.net.URLEncoder.encode(googleFormat0, "UTF-8"));
+                builder.append(java.net.URLEncoder.encode(googleFormat1, "UTF-8"));
+                builder.append(java.net.URLEncoder.encode(googleFormat2, "UTF-8"));
+                builder.append(java.net.URLEncoder.encode(googleFormat3, "UTF-8"));
+                builder.append(java.net.URLEncoder.encode(googleFormat4, "UTF-8"));
+                builder.append(java.net.URLEncoder.encode(googleFormat5, "UTF-8"));
+                builder.append(java.net.URLEncoder.encode(googleFormat6, "UTF-8"));
+            }
+            
+            final String location = builder.toString();
+            final String _respond = org.yccheok.jstock.gui.Utils.getResponseBodyAsStringBasedOnProxyAuthOption(location);
+            if (_respond == null) {
+                return java.util.Collections.emptyList();
+            }
+            final String respond = Utils.GoogleRespondToJSON(_respond);
+            // Google returns "// [ { "id": ... } ]".
+            // We need to turn them into "[ { "id": ... } ]".
+            final List<Map> jsonArray = gson.fromJson(respond, List.class);
+            
+            if (jsonArray == null) {
+                return java.util.Collections.emptyList();
+            }
+            
+            final List<Stock> stocks = new ArrayList<Stock>();
+            Set<Code> currCodes = new HashSet<Code>();
+            
+            for (int i = 0, size = jsonArray.size(); i < size; i++) {
+                final Map<String, String> jsonObject = jsonArray.get(i);
+                Pair<Stock, Boolean> stockEx = toStockEx(jsonObject, originalCodes);
+                if (stockEx != null) {
+                    final Stock stock = stockEx.first;
+                    if (stockEx.second) {
+                        if (false == currCodes.contains(stock.code)) {
+                            stocks.add(stock);
+                            currCodes.add(stock.code);
+                        }
+                    }
+                }
+            }  
+
+            return stocks;                
+        } catch (UnsupportedEncodingException ex) {
+            log.error(null, ex);
+            return java.util.Collections.emptyList();
+        } catch (Exception ex) {
+            log.error(null, ex);
+            // Jackson library may cause runtime exception if there is error
+            // in the JSON string.
+            return java.util.Collections.emptyList();
+        }        
+    }
+    
+    private boolean isUnitedStatesStockExchange(String e) {
+        return e.equals("NASDAQ") || e.equals("NYSE") ||
+               e.equals("NYSEARCA") || e.equals("NYSEMKT") || e.equals("OPRA") ||
+               e.equals("OTCBB") || e.equals("OTCMKTS");
+    }
+    
+    private Pair<Stock, Boolean> toStockEx(Map<String, String> jsonObject, Map<String, Code> originalCodes) {
+        String name;
+        final String ticker;
+        final String exchange;
+
+        try {
+            name = jsonObject.get("name");
+            name = name.substring(0, Math.min(SYMBOL_MAX_LENGTH, name.length())).trim();
+            ticker = jsonObject.get("t").toUpperCase();
+            exchange = jsonObject.get("e").toUpperCase();
+        } catch (Exception ex) {
+            log.error(null, ex);
+            return null;
+        }
+
+        Code code = getOriginalCode(originalCodes, ticker, exchange);
+        if (code == null) {
+            return null;
+        }
+
+        if (Utils.toCountry(code) == Country.UnitedState) {
+            if (false == isUnitedStatesStockExchange(exchange)) {
+                return Pair.create(Utils.getEmptyStock(code, Symbol.newInstance(code.toString())), false);
+            }
+        }
+        
+        double c = 0;
+        double l = 0;
+        double p = 0;
+        double op = 0;
+        double hi = 0;
+        double lo = 0;
+        long vo = 0;
+        double cp = 0;
+
+        // Change
+        try { c = Double.parseDouble(jsonObject.get("c").replaceAll("[^0-9\\.\\-]", "")); } catch (NumberFormatException ex) { log.error(null, ex); }
+        // Last
+        try {l = Double.parseDouble(jsonObject.get("l").replaceAll("[^0-9\\.]", "")); } catch (NumberFormatException ex) { log.error(null, ex); }
+        // Prev
+        try { p = l - c; } catch (NumberFormatException ex) { log.error(null, ex); }
+        // Open
+        try { op = Double.parseDouble(jsonObject.get("op").replaceAll("[^0-9\\.\\-]", "")); } catch (NumberFormatException ex) { log.error(null, ex); }
+        // High
+        try { hi = Double.parseDouble(jsonObject.get("hi").replaceAll("[^0-9\\.]", "")); } catch (NumberFormatException ex) { log.error(null, ex); }
+        // Low
+        try { lo = Double.parseDouble(jsonObject.get("lo").replaceAll("[^0-9\\.]", "")); } catch (NumberFormatException ex) { log.error(null, ex); }
+        // Vol
+        try { 
+            String vo_string = jsonObject.get("vo");
+            vo = (long)Double.parseDouble(vo_string.replaceAll("[^0-9\\.]", "")); 
+            if (vo_string.endsWith("K")) {
+                vo = vo * 1000;
+            } else if (vo_string.endsWith("M")) {
+                vo = vo * 1000000;
+            } else if (vo_string.endsWith("B")) {
+                vo = vo * 1000000000;
+            }
+        } catch (NumberFormatException ex) { log.error(null, ex); }
+        // Change Percentage
+        try { cp = Double.parseDouble(jsonObject.get("cp").replaceAll("[^0-9\\.\\-]", "")); } catch (NumberFormatException ex) { log.error(null, ex); }
+        // No last volumne information for Google Finance.
+        // No buy price information for Google Finance.
+        // No buy volume information for Google Finance.
+        // No sell price information for Google Finance.
+        // No sell volume information for Google Finance.                    
+        final Stock stock = new Stock.Builder(code, Symbol.newInstance(name))
+                .name(name)
+                .changePrice(c)
+                .lastPrice(l)
+                .prevPrice(p)
+                .openPrice(op)
+                .highPrice(hi)
+                .lowPrice(lo)
+                .volume(vo)
+                .changePricePercentage(cp)
+                .build();
+        return Pair.create(stock, true);
     }
     
     private static final int SYMBOL_MAX_LENGTH = 17;
