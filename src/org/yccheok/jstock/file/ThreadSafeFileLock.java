11862fb79e8c5e089339b168e6d9cd1b7bc64cb2
Yan Cheng Cheok <yccheok@yahoo.com>
2015-03-28 09:40:21 +0800

    Previously, our thread safe file locking mechanism only applicable to Statements class. Now, we want to make it available to other file type as well, like json.

---
+++ src/org/yccheok/jstock/file/ThreadSafeFileLock.java
@@ -0,0 +1,130 @@
+/*
+ * JStock - Free Stock Market Software
+ * Copyright (C) 2015 Yan Cheng Cheok <yccheok@yahoo.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package org.yccheok.jstock.file;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.yccheok.jstock.engine.Pair;
+
+/**
+ *
+ * Here's the reason why we didn't use Java's FileLock :
+ *
+ * File locks are held on behalf of the entire Java virtual machine. They are 
+ * not suitable for controlling access to a file by multiple threads within the 
+ * same virtual machine. File-lock objects are safe for use by multiple 
+ * concurrent threads.
+ * 
+ * http://stackoverflow.com/questions/10531286/is-filelock-in-java-safe-across-multiple-threads-within-the-same-process-or-betw
+ * 
+ */
+
+public class ThreadSafeFileLock {
+    
+    /***************************************************************************
+     * CRITICAL SECTION OF THE CODE.
+     **************************************************************************/
+    public Lock getLock(File file) {
+        String canonicalPath;
+        try {
+            canonicalPath = file.getCanonicalPath();
+        } catch (IOException ex) {
+            log.error(null, ex);
+            return null;
+        }
+        
+        Pair<ReentrantReadWriteLock, AtomicInteger> pair;
+        synchronized(reentrantReadWriteLockMapMonitor) {
+            pair = reentrantReadWriteLockMap.get(canonicalPath);
+            if (pair == null) {
+                ReentrantReadWriteLock reentrantReadWriteLock = new ReentrantReadWriteLock();
+                AtomicInteger atomicInteger = new AtomicInteger(1);
+                pair = Pair.create(reentrantReadWriteLock, atomicInteger);
+                reentrantReadWriteLockMap.put(canonicalPath, pair);
+            } else {
+                pair.second.incrementAndGet();
+            }
+        }
+        
+        Lock lock = new Lock(pair, canonicalPath);
+        return lock;
+    }
+    
+    public void releaseLock(Lock lock) {    
+        synchronized(reentrantReadWriteLockMapMonitor) {
+            int counter = lock.reentrantReadWriteLock.second.decrementAndGet();
+            if (counter == 0) {
+                reentrantReadWriteLockMap.remove(lock.canonicalPath);
+            }
+        } 
+    }
+    /***************************************************************************
+     * CRITICAL SECTION OF THE CODE.
+     **************************************************************************/
+    
+    /***************************************************************************
+     * CRITICAL SECTION OF THE CODE.
+     **************************************************************************/
+    public void lockRead(Lock lock) {
+        lock.reentrantReadWriteLock.first.readLock().lock();
+    }
+    
+    public void unlockRead(Lock lock) {
+        lock.reentrantReadWriteLock.first.readLock().unlock();
+    }
+    /***************************************************************************
+     * CRITICAL SECTION OF THE CODE.
+     **************************************************************************/
+
+    /***************************************************************************
+     * CRITICAL SECTION OF THE CODE.
+     **************************************************************************/
+    public void lockWrite(Lock lock) {
+        lock.reentrantReadWriteLock.first.writeLock().lock();
+    }
+    
+    public void unlockWrite(Lock lock) {
+        lock.reentrantReadWriteLock.first.writeLock().unlock();
+    }
+    /***************************************************************************
+     * CRITICAL SECTION OF THE CODE.
+     **************************************************************************/
+    
+    public static class Lock {
+        public final Pair<ReentrantReadWriteLock, AtomicInteger> reentrantReadWriteLock;
+        public final String canonicalPath;
+        
+        public Lock(Pair<ReentrantReadWriteLock, AtomicInteger> reentrantReadWriteLock, String canonicalPath) {
+            this.reentrantReadWriteLock = reentrantReadWriteLock;
+            this.canonicalPath = canonicalPath;
+        }
+    }
+    
+    private static final Map<String, Pair<ReentrantReadWriteLock, AtomicInteger>> reentrantReadWriteLockMap = new HashMap<>();
+    private static final Object reentrantReadWriteLockMapMonitor = new Object();
+    
+    private static final Log log = LogFactory.getLog(Statements.class);
+}
