a7e2a82edbbebdc5c83ada94bf7d3cdab2ed73b8
Yan Cheng Cheok <yccheok@yahoo.com>
2015-04-02 22:56:57 +0800

    1.0.7.4

--- src/org/yccheok/jstock/gui/Utils.java
+++ src/org/yccheok/jstock/gui/Utils.java
@@ -2711,11 +2711,11 @@ public class Utils {
     // Remember to update isCloudFileCompatible method.
     private static final int CLOUD_FILE_VERSION_ID = 1107;
 
-    private static final String ABOUT_BOX_VERSION_STRING = "1.0.7.3";
+    private static final String ABOUT_BOX_VERSION_STRING = "1.0.7.4";
 
-    // 1.0.7.3
+    // 1.0.7.4
     // For About box comparision on latest version purpose.
-    private static final int APPLICATION_VERSION_ID = 1132;
+    private static final int APPLICATION_VERSION_ID = 1133;
         
     private static final Executor zombiePool = Executors.newFixedThreadPool(Utils.NUM_OF_THREADS_ZOMBIE_POOL);
 
