263042b48561aaea1976b2106097be848bf24576
Yan Cheng Cheok <yccheok@yahoo.com>
2015-04-02 15:57:47 +0800

    Avoid using enum for Industry : http://stackoverflow.com/questions/29404648/technique-to-runtime-create-constant-which-are-memory-efficient

---
+++ src/org/yccheok/jstock/engine/Industry.java
@@ -0,0 +1,80 @@
+/*
+ * JStock - Free Stock Market Software
+ * Copyright (C) 2015 Yan Cheng Cheok <yccheok@yahoo.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package org.yccheok.jstock.engine;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ *
+ * @author yccheok
+ */
+public class Industry {
+    private Industry(String industry) {
+        this.industry = industry;
+    }
+    
+    public static Industry newInstance(String industry) {
+        if (industry == null) {
+            throw new java.lang.IllegalArgumentException("industry cannot be null");
+        }
+        
+        Industry result = map.get(industry);
+        if (result == null) {
+            final Industry instance = new Industry(industry);
+            result = map.putIfAbsent(industry, instance);
+            if (result == null) {
+                return instance;
+            }
+        }
+        
+        assert(result != null);
+        return result;
+    }
+    
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + industry.hashCode();
+        
+        return result;
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        }
+
+        if (!(o instanceof Board)) {
+            return false;
+        }
+        
+        return this.industry.equals(((Industry)o).industry);
+    }
+    
+    @Override
+    public String toString() {
+        return industry;
+    }
+    
+    // Avoid using interface. We want it to be fast!
+    private static final ConcurrentHashMap<String, Industry> map = new ConcurrentHashMap<>();
+    private final String industry;
+}
