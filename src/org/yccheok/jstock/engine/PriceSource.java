8df5fb6886b5d1f493b84b6fcf7bdf3a276da3d4
Yan Cheng Cheok <yccheok@yahoo.com>
2015-07-25 22:44:29 +0800

    org.yccheok.jstock.engine will be closed source component.

--- src/org/yccheok/jstock/engine/PriceSource.java
+++
@@ -1,9 +0,0 @@
-package org.yccheok.jstock.engine;
-
-public enum PriceSource {
-    // It is important that the used name shall be tally with StockServerFactory
-    // concrete class name.
-    Yahoo,
-    Google,
-    KLSEInfo;
-}
