45f0ff454ae85963e1a77208f94c9cbcf82888f3
Michael Rouse <michael@michaelrouse.net>
2015-09-05 07:55:39 -0500

    Do not minimize to system tray
    When constantly switching back and forth between windows, having JStock
    go to the system tray when minimized is EXTREMELY annoying and slows
    down productivity. Commenting out these two lines should remove this
    feature.

--- src/org/yccheok/jstock/gui/JStock.java
+++ src/org/yccheok/jstock/gui/JStock.java
@@ -433,7 +433,7 @@ public class JStock extends javax.swing.JFrame {
                 formWindowDeiconified(evt);
             }
             public void windowIconified(java.awt.event.WindowEvent evt) {
-                formWindowIconified(evt);
+                //formWindowIconified(evt);
             }
         });
         getContentPane().setLayout(new java.awt.BorderLayout(5, 5));
@@ -1083,7 +1083,7 @@ public class JStock extends javax.swing.JFrame {
 
         if (Utils.isWindows())
         {
-            this.setVisible(false);
+            //this.setVisible(false);
         }
     }//GEN-LAST:event_formWindowIconified
 
