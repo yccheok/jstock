ed0ff05f14ecd9d1eadb7f980e02ae01d366449f
Tee Shuwn Yuan <shuwnyuan@binary.com>
2016-08-15 13:39:32 +0800

    refactor portfolio task

--- src/org/yccheok/jstock/trading/PortfolioService.java
+++ src/org/yccheok/jstock/trading/PortfolioService.java
@@ -35,6 +35,57 @@ public class PortfolioService extends ScheduledService<Map<String, Object>> {
         public PortfolioTask() {
         }
 
+        private void getAccBlotter (String userID, String accountID) {
+            accBlotter = api.accountBlotter(userID, accountID);              
+            System.out.println("calling account Blotter DONE...");
+
+            // LOOP: call "get instrument" to get stock name for open positions + pending orders
+            LinkedTreeMap<String, Object> equity = (LinkedTreeMap) accBlotter.get("equity");
+            List<LinkedTreeMap<String, Object>> posList = (List) equity.get("equityPositions");
+
+            for (LinkedTreeMap<String, Object> pos : posList) {
+                String symbol = pos.get("symbol").toString();
+                if (instruments.containsKey(symbol)) {
+                    continue;
+                }
+
+                Map<String, Object> ins = api.getInstrument(pos.get("instrumentID").toString());
+                instruments.put(symbol, ins);
+            }
+
+            List<LinkedTreeMap<String, Object>> orders = (List) accBlotter.get("orders");
+            for (LinkedTreeMap<String, Object> ord : orders) {
+                String symbol = ord.get("symbol").toString();
+                if (instruments.containsKey(symbol)) {
+                    continue;
+                }
+
+                Map<String, String> param = new HashMap<>();
+                param.put("symbol", symbol);
+                List<Map<String, Object>> insList = api.searchInstruments(param);
+
+                for (Map<String, Object> ins : insList) {
+                    if (symbol.equals( ins.get("symbol").toString() )) {
+                        instruments.put(symbol, ins);
+                        break;
+                    }
+                }
+            }
+        }
+        
+        private Map<String, Double> getMarketPrices () {
+            // get latest prices for all symbols
+            ArrayList<String> symbols = new ArrayList<>(instruments.keySet());
+            List<Map<String, Object>> priceList = api.getMarketData(symbols, true);
+
+            Map<String, Double> prices = new HashMap<>();
+            for (Map<String, Object> price : priceList) {
+                prices.put(price.get("symbol").toString(), (Double) price.get("lastTrade"));
+            }
+            
+            return prices;
+        }
+        
         @Override
         protected Map<String, Object> call() throws Exception {
             Map<String, Object> result = new HashMap<>();
@@ -45,57 +96,16 @@ public class PortfolioService extends ScheduledService<Map<String, Object>> {
                 // only call account Blotter & get instruments during first run
                 if (needFullRefresh == true) {
                     needFullRefresh = false;
-                    
-                    accBlotter = api.accountBlotter(userID, accountID);              
-                    System.out.println("calling account Blotter DONE...");
 
-                    // LOOP: call "get instrument" to get stock name for open positions + pending orders
-                    LinkedTreeMap<String, Object> equity = (LinkedTreeMap) accBlotter.get("equity");
-                    List<LinkedTreeMap<String, Object>> posList = (List) equity.get("equityPositions");
-
-                    for (LinkedTreeMap<String, Object> pos : posList) {
-                        String symbol = pos.get("symbol").toString();
-                        if (instruments.containsKey(symbol)) {
-                            continue;
-                        }
-
-                        Map<String, Object> ins = api.getInstrument(pos.get("instrumentID").toString());
-                        instruments.put(symbol, ins);
-                    }
-
-                    List<LinkedTreeMap<String, Object>> orders = (List) accBlotter.get("orders");
-                    for (LinkedTreeMap<String, Object> ord : orders) {
-                        String symbol = ord.get("symbol").toString();
-                        if (instruments.containsKey(symbol)) {
-                            continue;
-                        }
-
-                        Map<String, String> param = new HashMap<>();
-                        param.put("symbol", symbol);
-                        List<Map<String, Object>> insList = api.searchInstruments(param);
-
-                        for (Map<String, Object> ins : insList) {
-                            if (symbol.equals( ins.get("symbol").toString() )) {
-                                instruments.put(symbol, ins);
-                                break;
-                            }
-                        }
-                    }
+                    getAccBlotter(userID, accountID);
                     result.put("accBlotter", accBlotter);
                     result.put("instruments", instruments);
 
                     System.out.println("DONE calling accBlotter & get instruments for positions / orders...");
                 } else {
-                    // get latest prices for all symbols
-                    ArrayList<String> symbols = new ArrayList<>(instruments.keySet());
-                    List<Map<String, Object>> priceList = api.getMarketData(symbols, true);
-
-                    Map<String, Double> prices = new HashMap<>();
-                    for (Map<String, Object> price : priceList) {
-                        prices.put(price.get("symbol").toString(), (Double) price.get("lastTrade"));
-                    }
-
+                    Map<String, Double> prices = getMarketPrices();
                     result.put("marketPrices", prices);
+                    
                     System.out.println("DONE calling get market data for positions / orders...");
                 }
             }
