afb97a37dcde5d9cbc875c6d35ac1f4f385005d6
Tee Shuwn Yuan <s.yuan31tee@gmail.com>
2016-10-04 16:53:47 +0800

    DW licence - show in Dialog webview

--- src/org/yccheok/jstock/gui/trading/SignIn.java
+++ src/org/yccheok/jstock/gui/trading/SignIn.java
@@ -5,6 +5,7 @@
  */
 package org.yccheok.jstock.gui.trading;
 
+import java.util.Optional;
 import javafx.beans.value.ObservableValue;
 import javafx.concurrent.Task;
 import javafx.concurrent.Worker;
@@ -17,6 +18,9 @@ import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar.ButtonData;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
@@ -64,10 +68,8 @@ public class SignIn {
 
     public void show () {
         initUI();
-        initEventHandler();
-        
-        // Drive Wealth Terms & Condition
-        //initLicenceTab();
+        signInHandler();
+        licenceHandler();
     }
 
     private void initUI () {
@@ -110,23 +112,20 @@ public class SignIn {
         
         GridPane.setHalignment(signInBtn, HPos.CENTER);
 
-        
-        /*
         // Licence
         HBox licenceHBox = new HBox(0);
         Label licenceLabel = new Label("By signing in you agree to ");
 
         licenceLabel.setPrefHeight(30);
-        SignIn.licenceLink.setPrefHeight(30);
+        licenceLink.setPrefHeight(30);
         HBox.setHgrow(licenceLabel, Priority.ALWAYS);
-        HBox.setHgrow(SignIn.licenceLink, Priority.ALWAYS);
+        HBox.setHgrow(licenceLink, Priority.ALWAYS);
 
-        licenceHBox.getChildren().addAll(licenceLabel, SignIn.licenceLink);
+        licenceHBox.getChildren().addAll(licenceLabel, licenceLink);
         // HBox max size will follow max size of all children nodes
         licenceHBox.setMaxSize(Region.USE_PREF_SIZE, Region.USE_PREF_SIZE );
-        grid.add(licenceHBox, 0, 6);
+        signInGrid.add(licenceHBox, 0, 6);
         GridPane.setHalignment(licenceHBox, HPos.CENTER);
-        */
 
         
         // make components auto resize
@@ -142,7 +141,7 @@ public class SignIn {
         stack.getChildren().add(signInGrid);
     }
     
-    private void initEventHandler () {
+    private void signInHandler () {
         // Sign In Button action
         signInBtn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
@@ -170,6 +169,50 @@ public class SignIn {
         });
     }
 
+    private void licenceHandler () {
+        final WebView browser = new WebView();
+        final WebEngine webEngine = browser.getEngine();
+
+        licenceLink.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {                
+                // Load DriveWealth T&C in Dialog - WebView
+                webEngine.load("https://drivewealth.com/terms-of-use/");
+                licenceLink.setDisable(true);
+
+                // show in Dialog
+                Dialog licenceDlg = new Dialog<>();
+                licenceDlg.setResizable(true);
+
+                ButtonType closeBtn = new ButtonType("Close", ButtonData.CANCEL_CLOSE);
+                licenceDlg.getDialogPane().getButtonTypes().add(closeBtn);
+
+                licenceDlg.getDialogPane().setContent(browser);
+                licenceDlg.showAndWait();
+
+                // Close btn is clicked, stop webEngine load
+                // http://stackoverflow.com/questions/22436498/how-to-stop-webengine-after-closing-stage-javafx
+                webEngine.load(null);
+            }
+        });
+
+        webEngine.getLoadWorker().stateProperty().addListener(
+            new javafx.beans.value.ChangeListener<Worker.State>() {
+                public void changed(ObservableValue ov, Worker.State oldState, Worker.State newState) {
+                    System.out.println("Loading licence page status: " + newState);
+
+                    if (newState == SUCCEEDED || newState == FAILED) {
+                        licenceLink.setDisable(false);
+
+                        if (newState == FAILED) {
+                            System.out.println("Failed loading licence page");
+                        }
+                    }
+                }
+            }
+        );
+    }
+    
     private Task createLoginTask (String userName, String password) {
         
         Task< Pair<SessionManager.Session, DriveWealth.Error> > loginTask = new Task< Pair<SessionManager.Session, DriveWealth.Error> >() {
@@ -215,67 +258,13 @@ public class SignIn {
         return loginTask;
     }
 
-    /*  
-    private void initLicenceTab () {
-        // Load Drive Wealth's licence in new Tab with browser
-        final WebView browser = new WebView();
-        final WebEngine webEngine = browser.getEngine();
-
-        SignIn.licenceTab.setText("Drive Wealth's Terms of Use");
-        SignIn.licenceTab.setContent(browser);
-        SignIn.licenceTab.setClosable(true);
-
-        final ProgressIndicator progressIn = new ProgressIndicator();
-        progressIn.setMaxSize(15, 15);
-        SignIn.licenceTab.setGraphic(progressIn);
-
-        SignIn.licenceLink.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent e) {
-                webEngine.load("https://drivewealth.com/terms-of-use/");
-                licenceLink.setDisable(true);
-                tabPane.getTabs().add(licenceTab);
-            }
-        });
-
-        webEngine.getLoadWorker().stateProperty().addListener(
-            new javafx.beans.value.ChangeListener<Worker.State>() {
-                public void changed(ObservableValue ov, Worker.State oldState, Worker.State newState) {
-                    if (newState == SUCCEEDED || newState == FAILED) {
-                        licenceLink.setDisable(false);
-
-                        if (progressIn.isVisible() == true) {
-                            progressIn.setVisible(false);
-                        }
-                        if (licenceTab.getGraphic() != null) {
-                            licenceTab.setGraphic(null);
-                        }
-
-                        if (newState == FAILED) {
-                            System.out.println("Failed loading licence page");
-                            return;
-                        }
-
-                        SingleSelectionModel<Tab> selectionModel = tabPane.getSelectionModel();
-                        selectionModel.select(licenceTab);
-                    }
-                    System.out.println("Loading licence page status: " + newState);
-                }
-            }
-        );
-    }
-    */
-    
-    
-    //private final Hyperlink licenceLink = new Hyperlink("Drive Wealth's Terms of Use");
+    private final Hyperlink licenceLink = new Hyperlink("Drive Wealth's Terms of Use");
     
     private final Button signInBtn = new Button("Sign in");
     private final TextField userField = new TextField();
     private final PasswordField pwdField = new PasswordField();
     private final Label errorText = new Label();
     
-    //private final Tab licenceTab = new Tab();
-
     private StackPane stack;
     private final GridPane signInGrid = new GridPane();
 }
